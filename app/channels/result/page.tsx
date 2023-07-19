1978d2c1a8cf373c835ac19f391a8c1ad44309a3
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-07-20 03:37:46 +0900

    変更 - 試しにPagesでParamを拾ってみる

--- app/channels/result/page.tsx
+++ app/channels/result/page.tsx
@@ -1,6 +1,16 @@
 import { ChannelSearchParams } from '@/channels/(types)';
 import { ChannelResult } from '@/channels/_components/Result/router';
 
+const createWhereQuery = (params: ChannelSearchParams) => {
+  return params.year;
+};
+
+const getChannelWhere = (params: ChannelSearchParams, page: string) => {
+  const query = createWhereQuery(params);
+
+  return query;
+};
+
 export default async function ChannelResultIndex({
   params,
   searchParams,
@@ -8,12 +18,11 @@ export default async function ChannelResultIndex({
   params: { pages: string };
   searchParams: ChannelSearchParams;
 }) {
+  const res = getChannelWhere(searchParams, '');
   return (
     <>
-      {searchParams.sort}
-      {searchParams.year}
-      {/* @ts-expect-error Async Server Component */}
-      <ChannelResult page={params.pages} params={searchParams} />
+      {res}
+      {/* <ChannelResult page={params.pages} params={searchParams} /> */}
     </>
   );
 }
