cfa27a6c152c1d9777e852a25717a30cef5c45fc
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-07-20 01:54:32 +0900

    変更 - コンソールログを仕込む

--- app/channels/_lib/api/getChannelWhere.ts
+++ app/channels/_lib/api/getChannelWhere.ts
@@ -2,26 +2,6 @@ import { getChannelWhereCount, getChannelWhereOffset } from '@/_utile/prisma';
 import { ChannelSearchParams } from '@/channels/(types)';
 import { Prisma } from '@prisma/client';
 
-export const getChannelWhere = async (
-  params: ChannelSearchParams,
-  page: string
-) => {
-  const query = createWhereQuery(params);
-  //モバイルでのみやすさも考慮し、20件ほどに絞る。10件だけはPCやタブレットで見るには少なすぎる
-  const BASE_QUERY_COUNT = 20;
-  //何も指定がないときのためのガード構文
-  const offsetNumber = page ? Number(page) : 1;
-
-  //ページ番号よりOffsetでずらす件数を作成し、+1することで、同じ要素の重複取得を防ぐ
-  const skip =
-    offsetNumber === 1 ? 0 : BASE_QUERY_COUNT * (offsetNumber - 1) + 1;
-
-  const res = await getChannelWhereOffset(skip, query, 'desc');
-  const count = await getChannelWhereCount(query);
-
-  return [res, count] as const;
-};
-
 const createWhereQuery = (
   params: ChannelSearchParams
 ): Prisma.ChannelWhereInput => {
@@ -30,6 +10,8 @@ const createWhereQuery = (
   const query = keys.reduce((acc, currentKey) => {
     //ページ数をParameterで
     if (currentKey[0] === 'orderBy') return acc;
+
+    console.log(currentKey[1]);
 
     //まずは配信時間のWhere文だけを作成する
     //後にタグ検索とかを行いたいので、改修はしようね
@@ -51,4 +33,24 @@ const createWhereQuery = (
   }, {});
 
   return query;
+};
+
+export const getChannelWhere = async (
+  params: ChannelSearchParams,
+  page: string
+) => {
+  const query = createWhereQuery(params);
+  //モバイルでのみやすさも考慮し、20件ほどに絞る。10件だけはPCやタブレットで見るには少なすぎる
+  const BASE_QUERY_COUNT = 20;
+  //何も指定がないときのためのガード構文
+  const offsetNumber = page ? Number(page) : 1;
+
+  //ページ番号よりOffsetでずらす件数を作成し、+1することで、同じ要素の重複取得を防ぐ
+  const skip =
+    offsetNumber === 1 ? 0 : BASE_QUERY_COUNT * (offsetNumber - 1) + 1;
+
+  const res = await getChannelWhereOffset(skip, query, 'desc');
+  const count = await getChannelWhereCount(query);
+
+  return [res, count] as const;
 };
